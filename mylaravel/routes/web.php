d04ca71417e19c8e75a00e3096f8d9cf93ce74fc
EzekielMorales <basszad3107@gmail.com>
2025-01-15 18:03:57 +0700

    Laravel Template
    login reg home

--- mylaravel/routes/web.php
+++ mylaravel/routes/web.php
@@ -4,20 +4,22 @@ use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\LaravelFormController;
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('home');
 });
 
 Route::get('/hello', function () {
     return "<h1>Hello, World!</h1>";
 });
-
+/*
 Route::get('/Christmas', function () {
     return "<h1>Merry Christmas , Santa claus HOHO !!!!🦌🎅🎄❄️☃️🎁</h1>";
 });
-
+*/
 
 Route::get('/LaravelForm',
 [LaravelFormController::class, 'showForm']);
 
 Route::post('/LaravelForm',
 [LaravelFormController::class, 'showForm']);
+
+
