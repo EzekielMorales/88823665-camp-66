e1a77b1632c948cbf957aeeb9654077ce2c81100
EzekielMorales <basszad3107@gmail.com>
2025-02-05 14:48:36 +0700

    Laravel - User Model
    - edit
    - delete
    - submit

--- mylaravel/routes/web.php
+++ mylaravel/routes/web.php
@@ -5,6 +5,7 @@ use App\Http\Controllers\LaravelFormController;
 use App\Http\Controllers\HomeController;
 use App\Http\Controllers\LoginController;
 use App\Http\Controllers\RegisterController;
+use App\Http\Controllers\UserController;
 
 
 Route::get('/login',
@@ -24,6 +25,22 @@ Route::get('/', function () {
     return view('home');
 });
 
+Route::post('/register',
+    [RegisterController::class, 'create']);
+
+
+Route::get('/users',
+    [UserController::class, 'index']);
+
+Route::get('/user/{id}',
+    [UserController::class, 'edit']);
+
+Route::put('/user',
+    [UserController::class, 'edit_action']);
+
+Route::delete('/user',
+    [UserController::class, 'delete']);
+
 
 
 /*
@@ -38,10 +55,18 @@ Route::get('/Christmas', function () {
 });
 */
 
+/* ตัวอย่างจากอาจารย์ ไม่ได้ถูกสร้าง */
+Route::get('/mycontroller/{id?}',
+    [MyController::class, 'myfuntion']);
+
+Route::get('/mycontroller/{id?}',
+    [MyController::class, 'myfuntion']);
+
+/* สำหรับ lab แม่สูตรคูณ  */
 Route::get('/LaravelForm',
-[LaravelFormController::class, 'showForm']);
+    [LaravelFormController::class, 'showForm']);
 
 Route::post('/LaravelForm',
-[LaravelFormController::class, 'showForm']);
+    [LaravelFormController::class, 'showForm']);
 
 
