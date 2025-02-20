8b15a6940fab5c9e3323dd22d0e465eced3039c5
EzekielMorales <basszad3107@gmail.com>
2025-02-20 18:24:40 +0700

    Login และ Dynamic Form-draft
    draft

--- mylaravel/app/Http/Controllers/ProductController.php
+++ mylaravel/app/Http/Controllers/ProductController.php
@@ -6,5 +6,21 @@ use Illuminate\Http\Request;
 
 class ProductController extends Controller
 {
-    //
+    function index() {
+        return view('product');
+    }
+    fuction add_product(Request $req){
+        $catagory = new Category();
+        $catagory->name = $req->category_name;
+        $catagory->save();
+
+        foreach($req->product_name as $value){
+            $product = new ProductList();
+            $product->name = $value;
+            $product->category_id = $catagory->id;
+            $product->user_id = session('user')->id;
+            $product->save();
+        }
+    }
+
 }
--- mylaravel/resources/views/product.blade.php
+++ mylaravel/resources/views/product.blade.php
@@ -3,25 +3,25 @@
 @section('content')
     <form action="{{ url('product') }}" method="post">
         @csrf
-        <div class="row">
+        <div class="row mt-3">
             <div class="col-6">
                 <div class="mb-3">
                     <label>Category Name</label>
-                    <input  name="category_name" type="text" class="form-control" />
+                    <input name="category_name" type="text" class="form-control" />
                 </div>
             </div>
         </div>
-        <button class="btn btn-primary" id="btn-add-product-list" type="button"> + เพิ่ม Product</button>
+        <button class="btn btn-primary mt-3" id="btn-add-product-list" type="button"> + เพิ่ม Product</button>
         <div class="row mt-3" id='product-list'>
-<div class="col-6" >
-<label>Product Name</label>
-</div>
+            <div class="col-6">
+                <label>Product Name <button type ="button"
+                        class="btn btn-danger ml-3 mt-2 mb-2 btn-del-product-list">ลบ</button></label>
+                <input name="product_name[]" type="text" class="form-control" />
+            </div>
         </div>
-        <div class="mt-3 row">
-            <button class="btn btn-success" type="submit">บันทึก</button>
-        </div>
+        <button type="submit" class="btn btn-success mt-3 mb-3">บันทึก</button>
     </form>
-    <table class="mt-3 table">
+    <table class="table">
         <thead>
             <tr>
                 <td>#</td>
@@ -37,23 +37,21 @@
 @endsection
 
 @section('scripts')
-<script>
-    $(document).ready(function(){
-        var count =1;
-        $('#btn-add-product').on('click', function(){
-            $("#add-product").append(`
-            <div class="mt-3 col-6">
-                <label class="form-label product-label">${count++}. Product Name
-                    <button type="button" class="btn btn-danger btn-delete-product">ลบ</button>
-                </label>
-                    <input type="text" name="product_name[]" class="form-control">
+    <script>
+        $(document).ready(function() {
+            $('#btn-add-product').on('click', function() {
+                $("#add-product").append(`
+            <div class="col-6">
+                <label>Product Name <button type ="button"
+                    class="btn btn-danger ml-3 mt-2 mb-2 btn-del-product-list">ลบ</button></label>
+                <input name="product_name[]" type="text" class="form-control" />
             </div>
             `)
-        })
+            })
 
-        $(document).on('click','.btn-delete-product', function(){
-            $(this).parent().parent().remove();
-        })
-    });
-</script>
+            $(document).on('click', '.btn-delete-product', function() {
+                $(this).parent().parent().remove();
+            })
+        });
+    </script>
 @endsection
